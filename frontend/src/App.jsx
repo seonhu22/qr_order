c1dab7b68cbcf2490c51c980cc01fce44e7cbe73
YoonJunHyeon <qmdldos@gmail.com>
2026-03-17 21:56:19 +0900

    chore : useQuery 등을 사용하기 전 Provider 세팅 관련 주석 변경

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,9 +6,11 @@ import AppRoutes from '@/shared/routes/AppRoutes';
 
 function App() {
   return (
-    // BrowserRouter는 라우팅을 위한 컴포넌트입니다. 앱 전체를 감싸서 라우팅 기능을 제공합니다.
+    // BrowserRouter는 라우팅을 위한 컴포넌트입니다.
+    // 앱 전체를 감싸서 라우팅 기능을 제공합니다.
     <BrowserRouter>
       {/* QueryClientProvider는 React Query를 위한 컨텍스트를 제공합니다. */}
+      {/* 이 Provider가 있어야 useQuery, useMutation, useQueryClient 등 훅을 사용할 수 있다. 이건 작성법이다.*/}
       <QueryClientProvider client={queryClient}>
         {/* AuthProvider는 인증 상태를 관리하는 컨텍스트를 제공합니다. */}
         <AuthProvider>
