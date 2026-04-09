5c741c2cdb037a84995d5b7c054dac830d20f657
seonhu22 <gclaris15@gmail.com>
2026-04-09 12:13:21 +0900

    feature: Param 자동 매핑 기능 추가 Param 자동 매핑 기능 추가 및 properties.example에 저장경로 예시, 라이브러리경로 예시 추가

--- qrorder/src/main/java/htms/QROrder/log/repository/LogMapper.java
+++ qrorder/src/main/java/htms/QROrder/log/repository/LogMapper.java
@@ -1,21 +1,20 @@
 package htms.QROrder.log.repository;
 
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface LogMapper {
-    public void insertLoginLog(@Param("uuid") String uuid,
-                               @Param("ip") String ip,
-                               @Param("successStatus") String successStatus,
-                               @Param("errMsg") String errMsg,
-                               @Param("userId") String userId,
-                               @Param("sysPlantCd") String sysPlantCd);
-    public void insertLogoutLog(@Param("uuid") String uuid);
-    public void insertMenuOpenAccessLog(@Param("menuUuid") String menuUuid,
-                                        @Param("logUuid") String logUuid,
-                                        @Param("menuCd") String menuCd);
-    public void insertMenuCloseAccessLog(@Param("menuUuid") String menuUuid);
+    public void insertLoginLog(String uuid,
+                                String ip,
+                                String successStatus,
+                                String errMsg,
+                                String userId,
+                                String sysPlantCd);
+    public void insertLogoutLog(String uuid);
+    public void insertMenuOpenAccessLog(String menuUuid,
+                                        String logUuid,
+                                        String menuCd);
+    public void insertMenuCloseAccessLog(String menuUuid);
     public void closeAllOpenSessions();
     public void closeAllOpenMenuSessions();
 }
