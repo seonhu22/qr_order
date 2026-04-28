2cc9f441cd309eab6b40800626fa5ade85cbc84a
Gomgom331 <122430711+Gomgom331@users.noreply.github.com>
2026-04-28 17:56:54 +0900

    feat: 파일첨부 컴포넌트 추가
    - 파일 업로드 입력 컴포넌트 추가
    - 첨부파일 다운로드 목록 컴포넌트 추가
    - 파일 제약 안내 컴포넌트 추가
    - 확장자별 파일 아이콘 매핑 추가
    - 파일첨부 타입과 스타일 정의

---
+++ frontend/src/shared/components/file-attachment/index.ts
@@ -0,0 +1,5 @@
+export { FileInputGroup } from './FileInputGroup';
+export { FileDownloadList } from './FileDownloadList';
+export { FileHint } from './FileHint';
+export type { ServerFile, FileChangeState, FileInputGroupProps, FileDownloadListProps } from './types';
+export type { FileHintVariant, FileHintProps } from './FileHint';
