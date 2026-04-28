00323cedf124f81282e72e9e00c6d82fcfcc8dcb
seonhu22 <gclaris15@gmail.com>
2026-04-28 17:37:17 +0900

    fix: ordNo 타입 변환 String to Integer

--- qrorder/src/main/java/htms/QROrder/system/domain/Menu.java
+++ qrorder/src/main/java/htms/QROrder/system/domain/Menu.java
@@ -13,7 +13,7 @@ public class Menu {
     @NotBlank(message = "오류가 발생했습니다. 관리자에게 문의해주세요.")
     private String parentMenuCd;
     @NotBlank(message = "오류가 발생했습니다. 관리자에게 문의해주세요.")
-    private String ordNo;
+    private Integer ordNo;
     @NotBlank(message = "오류가 발생했습니다. 관리자에게 문의해주세요.")
     private String treeLevel;
     private String menuUrl;
