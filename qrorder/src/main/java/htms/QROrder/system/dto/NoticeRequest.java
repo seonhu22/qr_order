0fba5ff3033a30f2c60d0921f99cb6fae16e0deb
seonhu22 <gclaris15@gmail.com>
2026-04-30 16:31:53 +0900

    fix: 공지사항 500 에러 관련 CRUD시 에러 발생 오류 수정 중

--- qrorder/src/main/java/htms/QROrder/system/dto/NoticeRequest.java
+++ qrorder/src/main/java/htms/QROrder/system/dto/NoticeRequest.java
@@ -1,14 +1,17 @@
 package htms.QROrder.system.dto;
 
-import com.sun.star.util.DateTime;
 import lombok.Data;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
 
 @Data
 public class NoticeRequest {
     private String sysId;
     private String noticeTitle;
     private String noticeDescription;
-    private DateTime startDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime startDate;
     private String deleteYn;
     private String useYn;
     private String fileUuid;
