3c4e442f3bbb1a894066e3c47868df6657f93bae
seonhu22 <gclaris15@gmail.com>
2026-06-04 10:36:18 +0900

    feature: 유저 비밀번호 초기화 기능, 테이블 GUI 기능 추가 및 초기 비밀번호 시스템변수화

---
+++ qrorder/src/main/java/htms/QROrder/client/dto/TableGuiItem.java
@@ -0,0 +1,16 @@
+package htms.QROrder.client.dto;
+
+import lombok.Data;
+
+@Data
+public class TableGuiItem {
+    private String sysId;
+    private String tableName;
+    private Integer tableNum;
+    private Integer tableQty;
+    private Integer xCoordinate;
+    private Integer yCoordinate;
+    private Integer height;
+    private Integer width;
+    private String tableType;
+}
