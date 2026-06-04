d97978b590589cfb9c77e2bec796581e86d82b5d
seonhu22 <gclaris15@gmail.com>
2026-06-04 14:30:00 +0900

    refactor: 사소한 오류들 수정 이름 오기 수정, 메서드 명 수정, 검색어 란에 조건 추가, 미사용 import 삭제, 빈배열 예외처리

--- qrorder/src/main/java/htms/QROrder/client/controller/PaymentManageController.java
+++ qrorder/src/main/java/htms/QROrder/client/controller/PaymentManageController.java
@@ -3,12 +3,9 @@ package htms.QROrder.client.controller;
 import htms.QROrder.auth.domain.Login;
 import htms.QROrder.client.dto.*;
 import htms.QROrder.client.service.*;
-import htms.QROrder.common.dto.CommonResponse;
-import htms.QROrder.common.dto.FileRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -35,11 +32,11 @@ public class PaymentManageController {
     @GetMapping("/history/detail/search/{masterSysId}")
     public List<PaymentInfoDetailResponse> getPaymentInfoDetail(@PathVariable String masterSysId) {
 
-        return paymentInfoDetailService.getPaymentInfoMaster(masterSysId);
+        return paymentInfoDetailService.getPaymentInfoDetail(masterSysId);
     }
 
     @GetMapping("/settlement/search")
-    public SettlementResponse getSettlement(@RequestParam SettlementRequest settlementRequest,
+    public SettlementResponse getSettlement(@RequestBody SettlementRequest settlementRequest,
                                 HttpSession session) {
 
         Login login = (Login) session.getAttribute("loginUser");
