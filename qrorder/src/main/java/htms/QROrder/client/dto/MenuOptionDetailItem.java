2e52ea11f8aef43f2dc2221e66055ef5748e92f8
seonhu22 <gclaris15@gmail.com>
2026-06-09 10:11:43 +0900

    feature: 옵션관리 수정

--- qrorder/src/main/java/htms/QROrder/client/dto/MenuOptionDetailItem.java
+++ qrorder/src/main/java/htms/QROrder/client/dto/MenuOptionDetailItem.java
@@ -9,6 +9,8 @@ public class MenuOptionDetailItem {
     private String menuOptionName;
     private String menuOptionPrice;
     private String menuDescription;
+    private String maximumYn;
+    private String maximumNum;
     private String useYn;
     private String fileUlid;
     private Integer ordNo;
