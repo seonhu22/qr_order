d7527e551fc029ef254700e850c85f96c9c985e4
seonhu22 <gclaris15@gmail.com>
2026-06-30 08:55:31 +0900

    fix: 주문현황 시간 항목 추가 Header쪽에 order_datetime은 최초 주문시간, update_datetime은 상태 변경할때 자동 업데이트니까 조리중에 조리 시작 시간 적고싶으면 update_datetime으로 불러오면 됨. 서빙완료나 취소쪽도 마찬가지

--- qrorder/src/main/java/htms/QROrder/client/dto/StatusItem.java
+++ qrorder/src/main/java/htms/QROrder/client/dto/StatusItem.java
@@ -1,8 +1,10 @@
 package htms.QROrder.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.util.List;
 
 @Data
@@ -18,6 +20,8 @@ public class StatusItem {
         private Integer orderNum;
         private Integer tableNum;
         private LocalDateTime orderDatetime;
+        @JsonFormat(pattern = "HH:mm")
+        private LocalTime orderTime;
         private String orderStatus;
     }
 
