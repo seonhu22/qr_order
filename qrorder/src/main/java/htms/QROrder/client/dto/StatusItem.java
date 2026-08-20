e9e5d288c44f430cae47230cc79a568542cf02ea
YoonJunHyeon <qmdldos@gmail.com>
2026-08-20 15:15:34 +0900

    fix : 최신 백앤드 상태로 롤백

--- qrorder/src/main/java/htms/QROrder/client/dto/StatusItem.java
+++ qrorder/src/main/java/htms/QROrder/client/dto/StatusItem.java
@@ -22,13 +22,13 @@ public class StatusItem {
         private String sysId;
         private Integer orderNum;
         private Integer tableNum;
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime orderDatetime;
+        @JsonFormat(pattern = "HH:mm")
+        private LocalTime orderDatetime;
         @JsonFormat(pattern = "HH:mm")
         private LocalTime orderTime;
         private String orderStatus;
-        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-        private LocalDateTime cancelDatetime;
+        @JsonFormat(pattern = "HH:mm")
+        private LocalTime cancelDatetime;
     }
 
     @Data
